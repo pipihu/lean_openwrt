7fcca8c102efcf4c40000d5c6e5fad375a961787
pipihu <pipihu@gmail.com>
2022-07-19 04:58:26 +0800

    Update: x86_64 diy-part2

--- x86_64/diy-part2.sh
+++ x86_64/diy-part2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i '/uci delete firewall.kms/,/uci commit firewall/d' feeds/luci/applications/luci-app-vlmcsd/root/etc/uci-defaults/luci-vlmcsd
+sed -i 's/	commit firewall/	set firewall.@rule[-1].enabled="0"\n&/' feeds/luci/applications/luci-app-vlmcsd/root/etc/uci-defaults/luci-vlmcsd
